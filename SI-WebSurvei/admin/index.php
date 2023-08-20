ce8599629e3b6d27671ebee534ab7a49f55d87a7
iqbalkurosaki <65084883+iqbalkurosaki@users.noreply.github.com>
2023-08-20 14:53:17 +0700

    upload smig-survei
    upload smig-survei SI-WebSurvei semen indonesia gresik

---
+++ SI-WebSurvei/admin/index.php
@@ -0,0 +1,10 @@
+halaman admin
+<br>
+<?php
+	session_start();
+	print_r($_SESSION);
+?>
+<br>
+<a href="kelolasurvey.php">Kelola Survey</a>
+<br>
+<a href="../logout.php">Logout</a>
---
+++ SI-WebSurvei/user/index.php
@@ -0,0 +1,10 @@
+halaman user
+<br>
+<?php
+	session_start();
+	print_r($_SESSION);
+?>
+<br>
+<a href="buatform.php">Buat form</a>
+<br>
+<a href="../logout.php">Logout</a>
